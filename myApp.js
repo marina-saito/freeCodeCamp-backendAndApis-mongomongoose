254a4f2ad71c93f1740c4985c6e151e0a0ee0dc2
marina <marina.ssaito@gmail.com>
2022-05-17 08:00:08 -0300

    feat: add mongodb and mongoose dependencies and connect to mongodb

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(`${process.env.MONGO_URI}/Cluster0`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
